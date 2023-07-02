284e5f3895c37e5ca399bb3884b6795fb523e59d
Michael Blydenburgh <michael.blydenburgh@libertymutual.com>
2023-07-02 10:23:05 -0400

    add an ECR

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs';
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Cluster } from "aws-cdk-lib/aws-ecs";
 import { Stack } from 'aws-cdk-lib';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export interface FargateFunInfrastructureProps {
 }
@@ -10,6 +11,10 @@ export class FargateFunInfrastructureStack extends Stack {
 
   constructor(scope: Construct, id: string, props: FargateFunInfrastructureProps = {}) {
     super(scope, id);
+
+    new Repository(this, "DockerRepository", {
+      repositoryName: "image_repository"
+    })
 
     const vpc = new Vpc(this, "VPC", {
       maxAzs: 3
